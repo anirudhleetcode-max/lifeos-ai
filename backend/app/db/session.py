ef2314dd504beb2fe3429b67cfb37a0d9c50bc6a
anirudhleetcode-max <anirudhleetcode@gmail.com>
2026-08-15 13:36:51 +0530

    Update session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -2,7 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://lifeos_user:lifeos_password@db:5432/lifeos_db"
+import os
+
+# This tells Python to use the Render Environment Variable, and if it fails, fallback to the Neon URL directly.
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "postgresql://neondb_owner:npg_jLZ5E4dRQvSc@ep-dry-flower-ax5325yu-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
